96d31235396d7d3a17a8dbb1b59f9e5a1771f96e
Daniil Antsupov <antsupov.official@gmail.com>
2021-05-08 16:53:41 +0700

    Update rabbitmq connection

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const MicroMQ = require('micromq');
 
-const app = new MicroMQ({
+const auth = new MicroMQ({
   name: 'auth',
+  rabbit: {
+    url: 'amqp://hasty:superhardpassword@localhost:5682',
+  },
 });
 
 // создаем два эндпоинта /friends & /status на метод GET
-app.get(['/login'], async (req, res) => {
+auth.get(['/login'], async (req, res) => {
   // делегируем запрос в микросервис users
   await res.delegate('users');
 });
 
-app.get('/login', (res, req) => {
+auth.get('/login', (res, req) => {
   res.json({ code: 401, text: 'wrong auth' });
 });
 
-app.start();
+auth.start();
