7ad0f276057a873e66f366b01ad81ee60cb7dcaa
Daniil Antsupov <antsupov.official@gmail.com>
2021-05-12 05:48:10 +0700

    dev Update routes

--- app.js
+++ app.js
@@ -1,36 +1,18 @@
 require('dotenv').config();
 
 const MicroMQ = require('micromq');
-const redis = require('redis');
-const { promisify } = require('util');
-
-const client = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
-});
-
-client.set('test', 'test');
-
-const getAsync = promisify(client.get).bind(client);
-
-const get = async () => {
-  let res = await getAsync('test');
-
-  console.log(res);
-};
-
-get();
 
 const app = new MicroMQ({
   name: 'auth',
   rabbit: {
-    url: 'amqp://hasty:superhardpassword@localhost:5682',
+    url: process.env.RABBIT_URL,
   },
 });
 
-app.get('/login', (req, res) => {
-  res.json({ code: 401, text: 'wrong auth' });
+app.get('/test', (req, res) => {
+  console.log(req);
+
+  res.json({ code: 200, text: 'Hello' });
 });
 
 app.start();
